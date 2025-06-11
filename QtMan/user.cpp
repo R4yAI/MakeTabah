b407fb62ea8e7232fc5fcaa21725f06ef3c55b4f
Rayan <rayan.ouali.2007@gmail.com>
2025-06-11 18:29:29 +0100

    added user class

---
+++ QtMan/user.cpp
@@ -0,0 +1,15 @@
+#include "user.h"
+#include<string>
+#include<chrono>
+#include<vector>
+
+user::user() {
+
+    long int id;
+    std::string userName;
+    std::string password;
+    bool admin;
+    std::chrono::time_point<std::chrono::system_clock> dueDate;
+
+
+}
