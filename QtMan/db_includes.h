ca8aa1c6bbf61686fed8606a08dfa856d031411a
Rayan <rayan.ouali.2007@gmail.com>
2025-06-12 11:12:36 +0100

    created Books database

---
+++ QtMan/db_includes.h
@@ -0,0 +1,13 @@
+#ifndef DB_INCLUDES_H
+#define DB_INCLUDES_H
+
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlTableModel>
+#include <QtSql/QSqlQueryModel>
+#include <QtSql/QSqlError>
+#include <QtSql/QSqlRecord>
+#include <QtSql/QSqlField>
+#include <QFile>
+
+#endif // DB_INCLUDES_H
