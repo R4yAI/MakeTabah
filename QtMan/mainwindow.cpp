c208b22aa5e731d09b76f972509081d8c4d672db
Rayan <rayan.ouali.2007@gmail.com>
2025-06-14 00:40:09 +0100

    added delete functionality to the database

--- QtMan/mainwindow.cpp
+++ QtMan/mainwindow.cpp
@@ -9,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->RsrcManWinBtn_3, &QPushButton::clicked, this, &MainWindow::on_RsrcManWinBtn_3_clicked);
     ptrRsrcManWin = new ResourceManagementWindow();
     ui->stackedWidget->addWidget(ptrRsrcManWin);
 
@@ -16,14 +17,9 @@ MainWindow::MainWindow(QWidget *parent)
 
 MainWindow::~MainWindow()
 {
-
+    delete ptrRsrcManWin;
     delete ui;
 }
-
-
-
-
-
 
 
 
